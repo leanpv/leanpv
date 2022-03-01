295b6a4b256362d0e37731c5a1d49e75657b823c
Lean <lean.puebla@gmail.com>
2022-03-01 20:54:59 -0300

    Updates

---
+++ precache-manifest.2ed5165b3133d267422289641f59ed5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59a86001af2a4ad98f9808490433443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1076b61adf80986646a",
+    "url": "/static/js/2.864006e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.864006e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa31d105ac75f6daf8fe",
+    "url": "/static/js/main.915befe3.chunk.js"
+  },
+  {
+    "revision": "52ca16f7ebe07f2f096f",
+    "url": "/static/js/runtime-main.40de8c13.js"
+  }
+]);
